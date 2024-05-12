77b3b4eb37c2ffa18292880c052c4f162aa44a15
AyrtonFabrizio <ayrtonfabrizioalvarez@gmail.com>
2024-05-12 10:32:23 -0300

    se acomodaron urls entre HU de mile, ivan y coco

--- sesiones/urls.py
+++ sesiones/urls.py
@@ -6,6 +6,16 @@ urlpatterns = [
     path('users/', views.list_users, name='list_users'),
     path('edit_user/<int:user_id>/', views.edit_user, name='edit_user'),
     path('delete_user/<int:user_id>/', views.delete_user, name='delete_user'),
-    path('delete/', views.delete, name='delete')
+    path('delete/', views.delete, name='delete'),
     
+    #MILENA
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('recuperarclave/', views.recuperar_contrasenia, name='recuperar_contrasenia'),
+    
+    #IVAN
+    path('signup_helper/', views.signup_helper, name='signup_helper'),
+    path('edit_helper/<int:helper_id>', views.edit_intern, name='edit_helper'),
+    path('helpers/', views.list_helpers, name='list_helpers'),
+    path('delete_helper/<int:helper_id>', views.delete_helper, name='delete_helper'),
 ]
