e8f5fb4ad7c6c5440d380a432bf5120b3ee233f6
Milena Confeggi <milenaconfeggi@gmail.com>
2024-05-26 09:16:39 -0300

    nose

--- listados/urls.py
+++ listados/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('agregar_categoria/', views.agregar_categoria, name='agregar_categoria'),
     path('listar_categorias/', views.listar_categorias, name='listar_categorias'),
     path('editar_categoria/<int:categoria_id>/', views.editar_categoria, name='editar_categoria'),
-    path('eliminar_categoria/<int:categoria_id>/', views.eliminar_categoria, name='eliminar_categoria')
+    path('eliminar_categoria/<int:categoria_id>/', views.eliminar_categoria, name='eliminar_categoria'),
     path('listar_intercambios_del_dia/', views.list_exchanges_today, name='listar_intercambios_del_dia'),
     path('listar_intercambios_del_dia/confirmar/<int:intercambio_id>/', intercambios_views.confirm_exchange, name='listar_intercambios_del_dia_confirmar'),
 ]
