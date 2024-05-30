a723ec52399d439d4a89e57f5d42c0e4450d3fb6
CamiiTKD <cami.tarrio2003@gmail.com>
2024-05-30 17:27:14 -0300

    correccion de error en rechazo

--- ofrecimientos/urls.py
+++ ofrecimientos/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('ver_ofrecimientos/<int:publicacion_id>', views.ver_ofrecimientos, name='ver_ofrecimientos'),
     path('aceptar_ofrecimiento/<int:ofrecimiento_id>', views.aceptar_ofrecimiento, name='aceptar_ofrecimiento'),
     path('rechazar_ofrecimiento/<int:ofrecimiento_id>', views.rechazar_ofrecimiento, name='rechazar_ofrecimiento'),
+    path('cancelar_operacion/<int:publicacion_id>', views.cancelar_operacion, name='cancelar_operacion'),
 ]
