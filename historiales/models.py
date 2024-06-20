9f67acee48dfac0f63eba82102a6035daf8a256c
Ivan Gonzalez <gonzalez.ivan.lp@gmail.com>
2024-06-20 11:05:23 -0300

    Actualizo base de datos (historial, favoritos)

--- historiales/models.py
+++ historiales/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from publicaciones.models import Publicacion
+from intercambios.models import Intercambio
 
-# Create your models here.
+class Historial(models.Model):
+    estado = models.CharField(max_length=100)
+    fecha = models.DateField()
+
+class PublicacionHistorial(models.Model):
+    publicacionId = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    historialId = models.ForeignKey(Historial, on_delete=models.CASCADE)
+
+class IntercambioHistorial(models.Model):
+    intercambioId = models.ForeignKey(Intercambio, on_delete=models.CASCADE)
+    historialId = models.ForeignKey(Historial, on_delete=models.CASCADE)
